5a618f812906c6df92616aa10750c6db11916855
levdong <levdong0108@gmail.com>
2023-02-23 00:29:38 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,7 @@ app.get("/",(req, res) => {
     res.json({ message: "Welcome to contact book application."});
 
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
 
